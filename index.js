564207e604fa0af86ab922256bd4346627e80b7c
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-10 18:54:59 -0300

    users changes

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
-//var MongoClient = require('mongodb').MongoClient;
-var multipart = require('connect-multiparty');
-//var session = require('express-session');
 const PORT = process.env.PORT || 5000
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
