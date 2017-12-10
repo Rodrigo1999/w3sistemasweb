cd597d25489b163577a5910b34d87a57c761daa9
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-10 19:05:29 -0300

    users changes

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.get('/', function(req, res){
 	res.render('home', {});
 
 })
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('db', {results: result.rows} ); }
+    });
+  });
+});
+
 http.listen(PORT, function(){
 console.log('listening on *:'+PORT);
 });
+
