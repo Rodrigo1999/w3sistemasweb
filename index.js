7b2e6981e2713165dfb158ad30e718d58b4c8c6c
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-11 00:56:59 -0300

    users changes

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000
 const path = require('path')
+var pg = require('pg');
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-	res.render('home', {});
+	res.render('home');
+	io.on('connection', function(socket){
+		socket.on('getDataPrimary', function(data){
+			
+			 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+			    client.query("insert into test_table values (2, '"+data.nome+"')", function(err, result) {
+			      done();
+			      if (err)
+			       { console.error(err); response.send("Error " + err); }
+			      else
+			       { socket.emit('getDataPrimary-Response', true); }
+			    });
+			  });
+			
+		})
+	})
 
 })
-
-var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
---
+++ public/js/global.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
+    return re.test(email);
+}
+
+function ucfirst(str) {
+	return str.substr(0,1).toUpperCase()+str.substr(1)
+}
