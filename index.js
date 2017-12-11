5db79e9f037418e0d557a55b28d952ae2aa0b4d4
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-11 12:02:26 -0300

    users changes

--- index.js
+++ index.js
@@ -35,17 +35,18 @@ app.get('/admin/db', function (request, response, next) {
 	
 	io.on('connection', function(socket){
 		socket.on('logindb', function(data){
-			request.user = {user: data, active: true};
-			pg.connect(process.env.DATABASE_URL, function(err, client, done){
-				client.query('SELECT * FROM admin WHERE login="'+data.login+'" and senha="'+data.senha+'"', function(err, result){
-					done();
-					if (err) {
-						{ console.error(err); response.send("Error " + err); }
-					}else{
-						socket.emit('count', result.rows);
-					}
-				});
-			})
+			//request.user = {user: data, active: true};
+			// pg.connect(process.env.DATABASE_URL, function(err, client, done){
+			// 	client.query('SELECT * FROM admin WHERE login="rodrigo" and senha="123"', function(err, result){
+			// 		done();
+			// 		if (err) {
+			// 			{ console.error(err); response.send("Error " + err); }
+			// 		}else{
+			// 			socket.emit('count', result.rows);
+			// 		}
+			// 	});
+			// })
+			socket.emit('count', data);
 			
 		})
 	})
