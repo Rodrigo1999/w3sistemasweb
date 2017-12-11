8476c8685c086ab8630fd764e227e6773c4a0b5c
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-11 13:46:45 -0300

    users changes

--- index.js
+++ index.js
@@ -40,27 +40,28 @@ app.get('/', function(req, res){
 app.get('/admin/db', function (request, response, next) {
 	var session = request.session;
 	response.render('admin', {passou: 'false'})
+	console.log(session.login);
 	
 	io.on('connection', function(socket){
 		socket.on('logindb', function(data){
-			request.user = {user: data, active: true};
-			pg.connect(process.env.DATABASE_URL, function(err, client, done){
-				client.query("select count(*) from admin where login='"+data.login+"' and senha='"+data.senha+"'", function(err, result){
-					done();
-					if (err) {
-						{ console.error(err); }
-					}else{
-						
-						if(result.rows[0].count == 1){
-							session.login = true;
-							response.send("Error ");
-							// next();
+			if(data.length == 2){
+				pg.connect(process.env.DATABASE_URL, function(err, client, done){
+					client.query("select count(*) from admin where login='"+data.login+"' and senha='"+data.senha+"'", function(err, result){
+						done();
+						if (err) {
+							{ console.error(err); }
 						}else{
-							socket.broadcast.emit('count', 'cai fora');
+							
+							if(result.rows[0].count == 1){
+								session.login = true;
+								next();
+							}else{
+								socket.broadcast.emit('count', 'cai fora');
+							}
 						}
-					}
-				});
-			})	
+					});
+				})	
+			}	
 		})
 	})
   /*pg.connect(process.env.DATABASE_URL, function(err, client, done) {
