8a3852809b52a9577730b7751356c2f1bb6707cb
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-11 12:04:53 -0300

    users changes

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ app.get('/admin/db', function (request, response, next) {
 			pg.connect(process.env.DATABASE_URL, function(err, client, done){
 				client.query('SELECT * FROM admin WHERE login="rodrigo" and senha="123"', function(err, result){
 					done();
-					
+					if (err) {
+						{ console.error(err); }
+					}else{
 						socket.emit('count', result.rows);
-					
+					}
 				});
 			})
 			
