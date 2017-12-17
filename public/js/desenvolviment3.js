1d5b599f02bce0c531a57fe112c6b86e84b4b459
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-17 13:00:22 -0300

    users changes

--- public/js/desenvolviment3.js
+++ public/js/desenvolviment3.js
@@ -1,14 +1,4 @@
-$(document).ready(function(){
-	var l = Ladda.create( document.querySelector( '.budget-message' ) );
-	$("#form-primary").submit(function(evt){evt.preventDefault();
-		var validate = true;
-		var data = {
-			nome: evt.target.nome.value,
-			email: evt.target.email.value,
-			telefone: evt.target.telefone.value,
-			celular: evt.target.celular.value,
-			mensagem: evt.target.mensagem.value
-		}
+$(document).ready(function(){var l = Ladda.create( document.querySelector( '.budget-message' ) ); $("#form-primary").submit(function(evt){evt.preventDefault(); var validate = true; var data = {nome: evt.target.nome.value, email: evt.target.email.value, telefone: evt.target.telefone.value, celular: evt.target.celular.value, mensagem: evt.target.mensagem.value }
 		var labels = ['nome', 'mensagem'];
 
 		if(!validateEmail(data.email)){
