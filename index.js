82ccc2a979a7463d3e035a6f704ea7c92f082857
Rodrigo Sousa <rodrigosousa1999rss@gmail.com>
2017-12-19 14:36:14 -0300

    users changes

--- index.js
+++ index.js
@@ -11,20 +11,17 @@ htmlentities=require('htmlentities');
 const PORT=process.env.PORT || 5000;
 
 
-function pb(d){
+var pb = function(d){
 	return htmlentities.encode(d);
 }
-function dc(d){
+var dc = function(d){
 	d=JSON.stringify(d);
 	d=htmlentities.decode(d);
 	return JSON.parse(d);
 }
-var directory=__dirname+'/views/readdingDbList.txt';
-if(fs.existsSync(directory)){
-	var file=fs.readFileSync(directory);
-}else{
-	var file=false;
-};
+
+var file=fs.readFileSync(__dirname+'/views/readdingDbList.txt');
+
 var session=require('express-session')({
 	secret: "my-secret",
     resave: true,
@@ -42,31 +39,14 @@ app.set('view engine','ejs');
 app.all('/',(req,res)=>{	
 	res.render('home',{date: d.getFullYear()});
 }).all('/admin/db',(req,res,next)=>{
-
-	var session=req.session;
-	if(session.login){
-		pg.connect(process.env.DATABASE_URL,(err,client,done)=>{
-		    client.query('SELECT * FROM budget_message order by id desc',(err,result)=>{
-			    done();
-			    if (err) { 
-			      	console.error(err); 
-			       	res.send("Error " + err); 
-			    }else { 
-			       	res.render('db',{results: dc(result.rows),count: result.rows.length} ); 
-			    }
-		    });
-		  });
-   	} else {
-     	
-      	res.render('admin');
-   	}
+	require('./libs/express/admin.js')(req,res,pg,dc);
 }).all('*',(req,res)=>{
   res.render('404',{date: d.getFullYear()});
 });
 var nspIndex=io.of('/index'),
 nspAdmin=io.of('/admin');
 nspIndex.on('connection',(socket)=>{
-	require('./libs/socket/index.js')(socket,nspIndex,pg,d,file);
+	require('./libs/socket/index.js')(socket,nspIndex,pg,d,file,pb);
 });
 
 nspAdmin.on('connection', (socket)=>{
